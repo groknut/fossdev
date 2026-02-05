4555bf6c19ab8556c85ec16efce112216838ec18
groknut <184639205+groknut@users.noreply.github.com>
2026-02-05 15:03:06 +0800

    update(main.py): commenting func()

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 
-# from pathlib import Path
-# 
-# src_path = Path(__file__).parent / "src"
-# 
-# src_path.mkdir(exist_ok=True)
-# print(src_path)
+"""
+Создание директории src
+"""
+from pathlib import Path
 
+src_path = Path(__file__).parent / "src"
+
+src_path.mkdir(exist_ok=True)
+print(src_path)
+
+"""
+Сумма двух чисел
+"""
 def sum(a: int, b: int) -> int:
     return a + b
 
+"""
+Деление двух чисел
+"""
 def div(a: int, b: int) -> int:
     if b == 0: raise ValueError("b == 0!")
     if isinstance(a, str) or isinstance(b, str): raise ValueError("wrong type")
