c52207332274ed387c99759c4d39465dc816e8e0
groknut <184639205+groknut@users.noreply.github.com>
2026-02-05 15:15:02 +0800

    add tests

---
+++ tests.py
@@ -0,0 +1,22 @@
+
+from main import sum, div
+
+"""
+Tests for sum() function
+"""
+def test_sum():
+    a, b = 1, 3
+    res = 3
+    assert sum(a, b) == res
+
+"""
+Tests for div() function
+"""
+def test_div():
+    a, b = 15, 3
+    res = 5
+    assert div(a, b) == res
+
+if __name__ == "__main__":
+    test_sum()
+    test_div()
