7283eea6d85afb8c036367ab9eb2cd5befd4332c
groknut <184639205+groknut@users.noreply.github.com>
2026-02-05 14:45:46 +0800

    update(main.py): add sum() & div()

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 
-from pathlib import Path
+# from pathlib import Path
+# 
+# src_path = Path(__file__).parent / "src"
+# 
+# src_path.mkdir(exist_ok=True)
+# print(src_path)
 
-src_path = Path(__file__).parent / "src"
+def sum(a: int, b: int) -> int:
+    return a + b
 
-src_path.mkdir(exist_ok=True)
-print(src_path)
+def div(a: int, b: int) -> int:
+    if b == 0: raise ValueError("b == 0!")
+    if isinstance(a, str) or isinstance(b, str): raise ValueError("wrong type")
+    return a / b
