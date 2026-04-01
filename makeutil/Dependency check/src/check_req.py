8945733ceab6692e10a2f24b95fb8637acd533a9
Monke <chikindin99@inbox.ru>
2026-04-01 03:40:09 +0000

    Added script for check requirements

---
+++ makeutil/Dependency check/src/check_req.py
@@ -0,0 +1,20 @@
+import sys
+
+def _check(file_req, file_lib):
+    req_map = {}
+    lib_vec = []
+    with open(file_req, "r", encoding = "utf-8") as req:
+        for r in req:
+            r = r.strip()
+            req_map[r] = True
+    
+    with open(file_lib, "r", encoding = "utf-8") as lib:
+        for l in lib:
+            l = l.strip().split(" ")
+            imp_idx = l.index("import")
+            if not req_map.get(l[imp_idx+1],False):
+                print("Error. File don't have library")
+            lib_vec.append(l[imp_idx+1])
+
+if __name__ == "__main__":
+    _check(sys.argv[1], sys.argv[2])
