e0d3a95218d17982c0a677455e911e54e9b33260
groknut <184639205+groknut@users.noreply.github.com>
2026-04-06 12:31:04 +0800

    Reformatted Makefile for chains task

--- makeutil/chains/src/calc.py
+++ makeutil/chains/src/calc.py
@@ -1,4 +1,5 @@
 def add(a: int, b: int) -> int:
     return a + b
 
+
 result: int = add(2, "3")
--- makeutil/chains/utils/check_req.py
+++ makeutil/chains/utils/check_req.py
@@ -1,20 +1,22 @@
 import sys
 
+
 def _check(file_req, file_lib):
     req_map = {}
     lib_vec = []
-    with open(file_req, "r", encoding = "utf-8") as req:
+    with open(file_req, "r", encoding="utf-8") as req:
         for r in req:
             r = r.strip()
             req_map[r] = True
-    
-    with open(file_lib, "r", encoding = "utf-8") as lib:
+
+    with open(file_lib, "r", encoding="utf-8") as lib:
         for l in lib:
             l = l.strip().split(" ")
             imp_idx = l.index("import")
-            if not req_map.get(l[imp_idx+1],False):
+            if not req_map.get(l[imp_idx + 1], False):
                 print("Error. Txt file don't have library")
-            lib_vec.append(l[imp_idx+1])
+            lib_vec.append(l[imp_idx + 1])
+
 
 if __name__ == "__main__":
     _check(sys.argv[1], sys.argv[2])
